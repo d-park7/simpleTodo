76cf2bd800ef6fe5485b1a6408b7d6329bb6b387
d-park7 <davidparkdp450@gmail.com>
2023-03-22 16:31:45 -0500

    bug with updates

--- api/routers/crud_user.py
+++ api/routers/crud_user.py
@@ -19,8 +19,18 @@ def create_user(user: schemas.UserInDB, db: Session = Depends(get_db)):
     db.refresh(db_user)
     return db_user
 
+@router.post("/update_users/{id}", response_description="Update a user", response_model=schemas.User)
+def update_user(user: schemas.User, db: Session = Depends(get_db)):
+    user_query = db.query(models.User).filter(models.User.id == id)
+    updated_user = user_query.first()
+    if not updated_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-@router.post("/user/{id}", response_description="Delete a user by email")
+    user_query.update(user)    
+    db.commit()
+    return updated_user
+
+@router.post("/delete_user/{id}", response_description="Delete a user by email")
 def delete_user(id: int, db: Session = Depends(get_db)):
     query_user = db.query(models.User).filter(models.User.id == id)
     if not query_user.first():
